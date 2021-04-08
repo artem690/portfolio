7cf3c10867c4e832b51384958708d433e3ae47c8
Artem <arne1063@colorado.edu>
2021-04-07 21:05:11 -0600

    added new project

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -8,6 +8,7 @@ import FILEBOX from "../../assets/img/projects/filebox.jpg";
 import COLORADO from "../../assets/img/projects/colorado.jpg";
 import SLACK from "../../assets/img/projects/slack.jpg";
 import TURN from "../../assets/img/projects/urturn.jpg";
+import TRIP from "../../assets/img/projects/trippla.jpg";
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 
@@ -33,6 +34,11 @@ import L_JAVA from "../../assets/img/skills/java.jpg";
 import L_SCALA from "../../assets/img/skills/scala.jpg";
 import L_ASSEMBLY from "../../assets/img/skills/assembly.jpg";
 import L_RAILS from "../../assets/img/skills/rails.jpg";
+
+import L_LAMBDA from "../../assets/img/skills/lambda.jpg";
+import L_ELASTIC from "../../assets/img/skills/elastic.jpg";
+import L_S3 from "../../assets/img/skills/s3.jpg";
+
 
 const Projects = () => {
    return (
@@ -462,7 +468,6 @@ const Projects = () => {
           </ImageEvent>
 
           {/* urturn */}
-          {/* SlackBot */}
           <ImageEvent
             date="8/2020-present"
             className="text-center"
@@ -566,6 +571,125 @@ const Projects = () => {
             
                 <UrlButton
                   href="https://github.com/ur-turn/accreditor"
+                  target="_blank"
+                >
+                  CLICK HERE FOR SOURCE CODE
+                </UrlButton>
+
+            </div>
+          </ImageEvent>
+
+
+           {/* Trip planner */}
+          <ImageEvent
+            date="01/2021-present"
+            className="text-center"
+            text="Trip Planner"
+            src={TRIP}
+            alt="Trip Planner"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                       <Button className="m-0" variant="outline-primary">PROJECT DETAILS</Button>
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong>Web application that helps user to get some helpful information for their future trip.
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>Using multiple APIs for data provision</li>
+                          <li>Get best flight prices to destination</li>
+                          <li>Get list with top 5 places to visit at destination</li>
+                          <li>Check when is the best time to go according to weather</li>
+                        </ul>
+                        
+                        <hr />
+                        <strong>Tech I used:</strong>
+                        <ul>
+                          <li>
+                           <span className="p-2">
+                                 <Image
+                                 src={L_LAMBDA}
+                                 alt="AWS Lambda"
+                                 rounded
+                                 className="image-style m-1"
+                                 ></Image>{" "}
+                                 AWS Lambda
+                              </span>
+                           </li>
+                           <li>
+                           <span className="p-2">
+                                 <Image
+                                 src={L_ELASTIC}
+                                 alt="AWS Elasticsearch"
+                                 rounded
+                                 className="image-style m-1"
+                                 ></Image>{" "}
+                                 AWS Elasticsearch
+                              </span>
+                           </li>
+                           <li>
+                           <span className="p-2">
+                                 <Image
+                                 src={L_S3}
+                                 alt="AWS S3"
+                                 rounded
+                                 className="image-style m-1"
+                                 ></Image>{" "}
+                                 AWS S3
+                              </span>
+                           </li>
+                           <li>
+                           <span className="p-2">
+                                 <Image
+                                 src={L_NODE_JS}
+                                 alt="JavaScript(Node)"
+                                 rounded
+                                 className="image-style m-1"
+                                 ></Image>{" "}
+                                 JavaScript(Node)
+                              </span>
+                           </li>
+                           <li>
+                           <span className="p-2">
+                                 <Image
+                                 src={L_REACT}
+                                 alt="React"
+                                 rounded
+                                 className="image-style m-1"
+                                 ></Image>{" "}
+                                 React
+                              </span>
+                           </li>
+                           <li>
+                           <span className="p-2">
+                                 <Image
+                                 src={L_GITHUB_PAGES}
+                                 alt="GitHub"
+                                 rounded
+                                 className="image-style m-1"
+                                 ></Image>{" "}
+                                 GitHub
+                              </span>
+                           </li>
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+            
+                <UrlButton
+                  href="https://github.com/CUBigDataClass/Trip-Planner"
                   target="_blank"
                 >
                   CLICK HERE FOR SOURCE CODE
